c35963959d404e0a5ae696ae1e8cfbc79349d397
jlotero23 <125835077+jlotero23@users.noreply.github.com>
2023-03-26 20:51:45 -0500

    getset

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -25,5 +25,17 @@ public class Fabricante {
 		
 		return masVendedor;
 	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.nombre = pais;
+	}
 	
 }
--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -5,5 +5,11 @@ public class Pais {
 	public Pais(String nombre){
 		this.nombre=nombre;
 	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 	
 }
