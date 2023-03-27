b7d3c399c19d1c362c41323e8d5f22c12918ea23
jlotero23 <125835077+jlotero23@users.noreply.github.com>
2023-03-26 21:18:34 -0500

    Update Pais.java

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,4 +1,5 @@
 package vehiculos;
+import java.util.ArrayList;
 
 public class Pais {
 	private String nombre;
@@ -11,5 +12,25 @@ public class Pais {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-}
+	public static Pais paisMasVendedor() {
+		ArrayList<Pais> paises = new ArrayList<Pais>();
+		for (Fabricante fabricante : Fabricante.fabricantes) {
+			paises.add(fabricante.getPais());
+			}
+		int mayor = 0;
+		Pais masVendedor = null;
+		for (Pais pais : paises) {
+			int cont = 0;
+			for (int i = 0; i < paises.size(); i++) {
+				if (pais == paises.get(i)){
+					cont++;
+					}
+				}
+			if (cont > mayor){
+				mayor = cont;
+				masVendedor = pais;
+				}
+			}
+		return masVendedor;
+		}
+	}
